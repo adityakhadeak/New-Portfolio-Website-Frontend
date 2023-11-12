a77df1d02f28081d07ed76c08f04f96e560599fd
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2023-11-12 21:52:55 +0530

    API url added

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
-// export const BASE_URL='https://quiknotebackend.onrender.com'
-export const BASE_URL='http://localhost:5000'
+// export const BASE_URL='http://localhost:5000'
+export const BASE_URL='https://adityakhadeakapis.vercel.app'
